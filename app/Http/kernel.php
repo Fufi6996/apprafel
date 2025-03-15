f44f695311182ee96ce2d0e5780c2a32d02e4d73
Rafel <restarellasmatas@cifpfbmoll.eu>
2025-03-15 23:12:15 +0100

    Agregar middleware para forzar respuestas JSON y configurar Sanctum para autenticación de API

--- app/Http/kernel.php
+++ app/Http/kernel.php
@@ -10,14 +10,17 @@ class Kernel extends HttpKernel
         // Middleware globales de Laravel
         \Illuminate\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
-        \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
+        //\App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \Illuminate\Foundation\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
     protected $middlewareAliases = [
-        'auth' => \App\Http\Middleware\Authenticate::class,
+        'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
+        'api' =>  \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        'throttle:api' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'json.response' => \App\Http\Middleware\ForceJsonResponse::class, // Si ForceJsonResponse es necesario
     ];
+
 }
